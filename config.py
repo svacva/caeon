a1283f7f1282e30d0c0b4ab2c9f397d0f6066da2
svacva <87240353+svacva@users.noreply.github.com>
2025-03-04 09:59:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,110 @@
+# REQUIRED CONFIG
+BOT_TOKEN = "7849957079:AAGiuJRHcoBiBJ8Cmwi86WFRX4PoqeIQl1k"
+OWNER_ID = 919042541
+TELEGRAM_API = 27731966
+TELEGRAM_HASH = "a94bbe4569ffcb3db064a59a40f2a7b8"
+
+# SEMI-REQUIRED, WE SUGGEST TO FILL IT FROM MONGODB
+DATABASE_URL = "mongodb+srv://cva5545:IUOHUwWxVdlDyzbH@cluster0.l031f.mongodb.net/?retryWrites=true&w=majority"
+
+# OPTIONAL CONFIG
+TG_PROXY = {}
+USER_SESSION_STRING = ""
+DOWNLOAD_DIR = "/usr/src/app/downloads/"
+CMD_SUFFIX = ""
+AUTHORIZED_CHATS = "-1004765589685"
+SUDO_USERS = ""
+DEFAULT_UPLOAD = "gd"
+FILELION_API = ""
+STREAMWISH_API = ""
+EXCLUDED_EXTENSIONS = ""
+INCOMPLETE_TASK_NOTIFIER = False
+YT_DLP_OPTIONS = ""
+USE_SERVICE_ACCOUNTS = False
+NAME_SUBSTITUTE = ""
+FFMPEG_CMDS = {}
+UPLOAD_PATHS = {}
+
+# INKYPINKY
+DELETE_LINKS = False
+FSUB_IDS = ""
+TOKEN_TIMEOUT = 0
+PAID_CHANNEL_ID = 0
+PAID_CHANNEL_LINK = ""
+SET_COMMANDS = True
+METADATA_KEY = "CVA - Telegram Channel @cvarips"
+WATERMARK_KEY = ""
+LOG_CHAT_ID = 0
+LEECH_FILENAME_CAPTION = ""
+HYDRA_IP = ""
+HYDRA_API_KEY = ""
+
+# GDrive Tools
+GDRIVE_ID = "1T3Esef6chJdSlyIoFFlygB7A-UwNpRjp"
+IS_TEAM_DRIVE = False
+STOP_DUPLICATE = True
+INDEX_URL = "https://cvam.teamhdt1.workers.dev/0:/mltb/"
+
+# Rclone
+RCLONE_PATH = ""
+RCLONE_FLAGS = ""
+RCLONE_SERVE_URL = ""
+RCLONE_SERVE_PORT = 0
+RCLONE_SERVE_USER = ""
+RCLONE_SERVE_PASS = ""
+
+# Mega credentials
+MEGA_EMAIL = ""
+MEGA_PASSWORD = ""
+
+# Sabnzbd
+USENET_SERVERS = [
+    {
+        "name": "main",
+        "host": "",
+        "port": 563,
+        "timeout": 60,
+        "username": "",
+        "password": "",
+        "connections": 8,
+        "ssl": 1,
+        "ssl_verify": 2,
+        "ssl_ciphers": "",
+        "enable": 1,
+        "required": 0,
+        "optional": 0,
+        "retention": 0,
+        "send_group": 0,
+        "priority": 0,
+    },
+]
+
+# Update
+UPSTREAM_REPO = "https://github.com/AeonOrg/Aeon-MLTB"
+UPSTREAM_BRANCH = "main"
+
+# Leech
+LEECH_SPLIT_SIZE = 0
+AS_DOCUMENT = True
+MEDIA_GROUP = False
+USER_TRANSMISSION = False
+HYBRID_LEECH = False
+LEECH_FILENAME_PREFIX = ""
+LEECH_DUMP_CHAT = "-1002349997875"
+THUMBNAIL_LAYOUT = ""
+
+# qBittorrent/Aria2c
+TORRENT_TIMEOUT = 0
+BASE_URL = "http://185.170.112.86:38"
+BASE_URL_PORT = 38
+WEB_PINCODE = False
+
+# Queueing system
+QUEUE_ALL = 0
+QUEUE_DOWNLOAD = 0
+QUEUE_UPLOAD = 0
+
+# RSS
+RSS_DELAY = 600
+RSS_CHAT = ""
+RSS_SIZE_LIMIT = 0
